c89666408eb37070a536147dc642d89b4eb0b720
taoweipu <taoweipu@evergrande.com>
2020-10-26 14:50:49 +0800

    提交demo的common组件返回状态码

---
+++ common/src/main/java/constant/StatusCode.java
@@ -0,0 +1,14 @@
+package constant;
+
+/**
+ * 定义返回响应码
+ * @author taoweipu
+ *
+ */
+public class StatusCode {
+
+    public static final int SUCCESS=10000;
+
+    public static final int FAIL=20000;
+
+}
